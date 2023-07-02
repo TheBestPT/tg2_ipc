2b86c5703011195766fd9f24e90705b2aeadb2ff
TheBestPT <thebestptnmo@gmail.com>
2023-07-02 19:47:13 +0100

    Minor fix

--- app/src/main/java/com/example/finalproject/TTS.java
+++ app/src/main/java/com/example/finalproject/TTS.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.speech.tts.TextToSpeech;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class TTS {
@@ -20,7 +22,7 @@ public class TTS {
         }
         tts = new TextToSpeech(context, (status) -> {
             if (status != TextToSpeech.ERROR) {
-                tts.setLanguage(Locale.ENGLISH);
+                tts.setLanguage(Locale.FRENCH);
             } else {
                 Toast.makeText(context, "Erro", Toast.LENGTH_SHORT).show();
             }
